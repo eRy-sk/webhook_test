dc11d2b9e0694726136429b6bd53029bd0ca3d94
Ryosuke Hiroe <ride.poke@gmail.com>
2018-09-10 12:29:20 +0900

    webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -11,7 +11,8 @@ class WebhookController < ApplicationController
   def hook
     # ダイジェスト値をBASE64エンコードした文字列が、リクエストヘッダに付与された署名（`X-ChatWorkWebhookSignature`ヘッダ、もしくはリクエストパラメータ`chatwork_webhook_signature`の値）と一致することを確認
     if chatwork_signature.present? && chatwork_signature == Base64.strict_encode64(digest)
-      ChatWork::Message.create(room_id: '122180676', body: params[:body])
+      puts body = JSON.parse(request.body.read, {:symbolize_names => true})
+      # ChatWork::Message.create(room_id: '122180676', body: body)
       head :ok
     else
       render text: 'invalid', status: 403
