a65b4f8cce9c55b09fdd58a0d446fc7bf0ef49dc
Ryosuke Hiroe <ride.poke@gmail.com>
2018-09-10 10:53:01 +0900

    webhook

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/sendmessage', to: 'webhook#sendMessage'
-  post '/hook', to: 'webhook#hook'
+  get '/hook', to: 'webhook#hook'
 end
