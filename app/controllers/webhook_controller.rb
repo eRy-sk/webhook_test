f2b19a62b0e13031d0742d72c75153e3390b41ae
Ryosuke Hiroe <ride.poke@gmail.com>
2018-09-10 10:23:02 +0900

    webhook

--- app/controllers/webhook_controller.rb
+++ app/controllers/webhook_controller.rb
@@ -6,8 +6,10 @@ class WebhookController < ApplicationController
     redirect_to action: 'home'
   end
   def hook
+    # ダイジェスト値をBASE64エンコードした文字列が、リクエストヘッダに付与された署名（`X-ChatWorkWebhookSignature`ヘッダ、もしくはリクエストパラメータ`chatwork_webhook_signature`の値）と一致することを確認
     if chatwork_signature.present? && chatwork_signature == Base64.strict_encode64(digest)
       render text: 'success', status: 200
+      render text: request_params, status: 200
     else
       render text: 'invalid', status: 403
     end
@@ -16,11 +18,14 @@ class WebhookController < ApplicationController
   private
 
     def secret_key
+      # リクエストの署名検証に使う秘密鍵
+      # トークンをBASE64デコードしたバイト列
       token = ENV['WEBHOOK_TOKEN']
       Base64.decode64(token)
     end
 
     def request_body
+      # チャットワークのWebhookからのHTTPSリクエストボディをそのままの文字列で取得
       request.body.read
     end
 
@@ -29,6 +34,10 @@ class WebhookController < ApplicationController
     end
 
     def digest
+      # 取得したHTTPSリクエストボディと秘密鍵をつかって、HMAC-SHA256アルゴリズムによりダイジェスト値を取得
       OpenSSL::HMAC.digest(OpenSSL::Digest.new('sha256'), secret_key, request_body)
+    end
+    def request_params
+      @params ||= JSON.parse(request.body.read, {:symbolize_names => true})
     end
 end
